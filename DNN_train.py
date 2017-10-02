746ade166533827a05042098a3895dfd621fcf70
Havukainen Joona J <joona.havukainen@helsinki.fi>
2017-10-02 14:14:59 +0300

    Check if the folder for Tensorflow_graph exists already and overwrite

--- DNN_train.py
+++ DNN_train.py
@@ -17,8 +17,9 @@ import pandas as pd
 import numpy as np
 import keras.callbacks
 import glob
-
 from sklearn.metrics import roc_auc_score
+
+K.set_learning_phase(0)
 
 #/////////////////TO BE MOVED INTO SEPARATE FILE FOR CLARITY
 #ROC value to be printed out after epochs. Does not affect training
@@ -50,36 +51,21 @@ listOfFiles=glob.glob("/work/hajohajo/DNNTrackReconstruction/trackingNtuples_TRA
 
 #List of features not to be used in the training
 #ignores=['trk_algoMask','vtx*','bsb*','simvtx*','simpv*','event','lumi','run','trk_mva','trk_isHP','trk_algo','trk_qualityMask','trk_nValid','trk_q','trk_vtxIdx','__array_index']
-read=['trk_isTrue','trk_pt','trk_ptErr','trk_nInnerLost','trk_nOuterLost','trk_nPixel','trk_nStrip','trk_eta','trk_nChi2','trk_nChi2_1Dmod','trk_n3DLay','trk_nLostLay','trk_nPixelLay','trk_nStripLay','trk_ndof']
+#read=['trk_isTrue','trk_pt','trk_ptErr','trk_nInnerLost','trk_nOuterLost','trk_nPixel','trk_nStrip','trk_eta','trk_nChi2','trk_nChi2_1Dmod','trk_n3DLay','trk_nLostLay','trk_nPixelLay','trk_nStripLay','trk_ndof']
+read=['trk_isTrue','trk_pt','trk_eta','trk_lambda','trk_dxy','trk_dz','trk_dxyClosestPV','trk_dzClosestPV',
+	'trk_ptErr','trk_etaErr','trk_lambdaErr','trk_dxyErr','trk_dzErr','trk_nChi2','trk_ndof','trk_nInvalid',
+	'trk_nPixel','trk_nStrip','trk_nPixelLay','trk_nStripLay','trk_n3DLay','trk_nLostLay','trk_algo'] 
 
 #data=read_root(listOfFiles,ignore=ignores,flatten=True)
-data=read_root(listOfFiles,columns=read,flatten=True)
+data=read_root(listOfFiles[1:],columns=read,flatten=True)
 print('read data to dataframe')
-
+print listOfFiles[0]
+data.drop('__array_index',axis=1,inplace=True)
+print(data.columns)
 targets=data['trk_isTrue']
-variables=['trk_pt','trk_lostmidfrac','trk_minlost','trk_nhits','trk_relpterr','trk_eta','trk_chi2n_n1dmod','trk_chi2n','trk_nlayerslost','trk_nlayers3D','trk_nlayers','trk_ndof']
-
-newdf=pd.DataFrame(columns=variables)
-dum_=pd.DataFrame(columns=['trk_pt'],data=np.ones(data.shape[0])*0.000001)
-
-newdf['trk_relpterr']=data['trk_ptErr'].divide(pd.DataFrame([data['trk_pt'],dum_['trk_pt']]).max())
-newdf['trk_pt']=data['trk_pt']
-newdf['trk_nhits']=data['trk_nPixel']+data['trk_nStrip']
-newdf['trk_minlost']=pd.DataFrame([data['trk_nInnerLost'],data['trk_nOuterLost']]).min()
-newdf['trk_lostmidfrac']=(data['trk_nInnerLost']+data['trk_nOuterLost']).divide(newdf['trk_nhits']+(data['trk_nInnerLost']+data['trk_nOuterLost']))
-newdf['trk_eta']=data['trk_eta']
-newdf['trk_chi2n_n1dmod']=data['trk_nChi2_1Dmod']
-newdf['trk_chi2n']=data['trk_nChi2']
-newdf['trk_nlayers3D']=data['trk_n3DLay']
-newdf['trk_nlayers']=data['trk_nPixelLay']+data['trk_nStripLay']
-newdf['trk_nlayerslost']=data['trk_nLostLay']
-newdf['trk_ndof']=data['trk_ndof']
-
-train=newdf.sample(frac=0.95,random_state=200)
-test=newdf.drop(train.index)
-
-print train
-#print test
+data.drop('trk_isTrue',axis=1,inplace=True)
+train=data.sample(frac=0.9,random_state=7)
+test=data.drop(train.index)
 
 #train=data.sample(frac=0.95,random_state=200).drop(['trk_isTrue'],1)
 #test=data.drop(train.index).drop(['trk_isTrue'],1)
@@ -98,8 +84,8 @@ x_test=np.array(test.iloc[:])
 y_test=np.array(test_targets.iloc[:])
 
 #Defining the network topology
-dropoutRate=0.02
-a_inp = Input(shape=(x_train.shape[1],))
+dropoutRate=0.04
+a_inp = Input(shape=(x_train.shape[1],),name='ins')
 a = Dense(300,activation='relu', kernel_initializer='normal')(a_inp)
 a = Dropout(dropoutRate)(a)
 a = Dense(150,activation='relu', kernel_initializer='normal')(a)
@@ -107,9 +93,9 @@ a = Dropout(dropoutRate)(a)
 a = Dense(20,activation='relu', kernel_initializer='normal')(a)
 a = Dropout(dropoutRate)(a)
 a = Dense(10,activation='relu', kernel_initializer='normal')(a)
-a_out = Dense(1, activation='softsign', kernel_initializer='normal')(a)
+a_out = Dense(1, activation='sigmoid', kernel_initializer='normal',name='outs')(a)
 
-model=Model(a_inp,a_out)
+model=Model(inputs=a_inp,outputs=a_out)
 model.compile(loss=loss_,optimizer='Adam',metrics=['acc'])
 
 
@@ -117,16 +103,21 @@ cb=ROC_value()
 loss=LossHistory()
 check=keras.callbacks.ModelCheckpoint('KERAS_best_model_'+loss_+'.h5',monitor='val_loss',save_best_only=True)
 class_weight = class_weight.compute_class_weight('balanced', np.unique(y_train[:]),y_train[:])
-Nepoch=80
+Nepoch=100
 batchS=512
 model.fit(x_train,y_train,
 	epochs=Nepoch,
 	batch_size=batchS,
 	class_weight=class_weight,
 	callbacks=[cb,loss,check],
-	validation_split=0.05)
+	validation_split=0.1,
+	shuffle=True)
 
 model.save('my_model_'+loss_+'_Adam.h5')
+
+import os
+if os.path.exists("./Tensorflow_graph"):
+  os.system("rm -rf ./Tensorflow_graph")
 
 builder = tf.saved_model.builder.SavedModelBuilder("./Tensorflow_graph")
 builder.add_meta_graph_and_variables(sess,[tf.saved_model.tag_constants.SERVING])
